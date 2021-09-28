4a9eb30cdd8efa42dab836af7d7932cc72a838a9
priyanka897-star <priyankamore8997@gmail.com>
2021-09-28 21:05:18 +0530

    Added:Flip Coin and calculate  head and tail Percentage

---
+++ basicjavaprograms/FindLargestNumber.java
@@ -0,0 +1,25 @@
+package basicjavaprograms;
+import java.util.Scanner;
+
+public class FindLargestNumber {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Three Numbers :");
+		int number1 = scanner.nextInt();
+		int number2 = scanner.nextInt();
+		int number3 = scanner.nextInt();
+		if(number1 > number2 && number1 > number3)
+		{
+			System.out.println("Largest Number is :" + number1);
+		}	
+		else if(number2 > number1 && number2 > number3)
+		{	
+			System.out.println("Largest Number is :" + number2);
+		}
+		else
+		{
+			System.out.println("Largest Number is :" + number3);
+		}	
+	}
+
+}
---
+++ basicjavaprograms/PowerOf2.java
@@ -0,0 +1,28 @@
+package basicjavaprograms;
+import java.util.Scanner;
+
+public class PowerOf2 {
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a Number less than 31 to print power of 2 from 1 to N:");
+		int number = scanner.nextInt();
+		if(number > 0 && number < 31)
+		{
+		double powerValue = Math.pow(2, number);
+		System.out.println(powerValue);
+		
+		for(int i = 0; i <= number; i++)
+		{
+			System.out.println("2^" + i + "=" + Math.pow(2, i));
+		}
+	
+		}
+		else {
+			System.out.println("OVerflow an Interger");
+		}
+
+	}
+
+
+}
